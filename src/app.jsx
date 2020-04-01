cc1863b209e9e22fde3ea6c339a976d5bcc70e1f
Coding-Christian <christianhaney1996@gmail.com>
2020-04-01 11:45:12 -0700

    made app stateless

--- src/app.jsx
+++ src/app.jsx
@@ -2,25 +2,15 @@ import React from 'react';
 import SpeedCalculator from './speedCalculator';
 import TimeCalculator from './timeCalculator';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      readingSpeed: 0,
-      pages: 0,
-      bookSize: 0
-    };
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="row border p-2 m-2">
-          <TimeCalculator />
-          <SpeedCalculator />
-        </div>
+function App(props) {
+  return (
+    <div className="container">
+      <div className="row border p-2 m-2">
+        <TimeCalculator />
+        <SpeedCalculator />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
